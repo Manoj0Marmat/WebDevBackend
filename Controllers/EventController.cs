21a469a0c7da8f4e22425aee2cdddcfaf8c34956
Manoj Marmat <manojrajput762000@gmail.com>
2023-05-26 02:12:17 +0530

    Add: event controller

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -6,47 +6,49 @@ using Microsoft.AspNetCore.Mvc;
 using WebDevBackend.Dtos.Event;
 using WebDevBackend.Models;
 
+
 namespace WebDevBackend.Controllers
 {
     [ApiController]
     [Route("api/v3/app/events")]
     public class EventController : ControllerBase
     {
-        private static List<Event> events = new List<Event>{
-            new Event(),
-            new Event{Id = 1}
-        };
+        private readonly IEventService _eventService;
+
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
 
         [HttpGet("{id}")]
-        public IActionResult GetEventById(int id)
+        public async Task<ActionResult> GetEventById(int id)
         {
-            return Ok(events.FirstOrDefault(c => c.Id == id));
+            return Ok(await _eventService.GetById(id));
         }
 
         [HttpGet]
-        public IActionResult GetLatestEvents(string type, int limit, int page)
+        public async Task<ActionResult> GetLatestEvents(string type, int limit, int page)
         {
-            return Ok(events);
+            return Ok(await _eventService.GetAllEvents());
         }
 
         [HttpPost]
-        public ActionResult<List<Event>> AddEvent([FromForm]Event newEvent)
+        public async Task<ActionResult<List<Event>>> AddEvent([FromForm]Event newEvent)
         {
-            events.Add(newEvent);
-            return Ok(events);
+            return Ok(await _eventService.AddEvent(newEvent));
         }
 
-        [HttpPut("{id}")]
-        public IActionResult UpdateEvent(UpdateEventDto updateEventDto)
-        {
-            return Ok($"Event with ID: {updateEventDto.Id} updated successfully");
-        }
+        // [HttpPut("{id}")]
+        // public IActionResult UpdateEvent(UpdateEventDto updateEventDto)
+        // {
+        //     return Ok($"Event with ID: {updateEventDto.Id} updated successfully");
+        // }
 
 
-        [HttpDelete("{id}")]
-        public IActionResult DeleteEvent(int id)
-        {
-            return Ok($"Event with ID: {id} deleted successfully");
-        }
+        // [HttpDelete("{id}")]
+        // public IActionResult DeleteEvent(int id)
+        // {
+        //     return Ok($"Event with ID: {id} deleted successfully");
+        // }
     }
 }
